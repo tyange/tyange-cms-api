568457051f069e4acf7e08a19de4298505b0028f
tyange <usun16@gmail.com>
2025-08-12 15:02:13 +0900

    add test for generate token

---
+++ tests/generate_token_test.rs
@@ -0,0 +1,14 @@
+use std::env;
+
+use dotenv::dotenv;
+use tyange_cms_backend::auth::jwt::Claims;
+
+#[test]
+fn test_generate_token() {
+    dotenv().ok();
+
+    let secret = env::var("JWT_ACCESS_SECRET").unwrap();
+    let claims = Claims::new("test", "access", 15);
+    let access_token = claims.to_token(&secret.as_bytes()).unwrap();
+    println!("Generated token: {}", access_token);
+}
